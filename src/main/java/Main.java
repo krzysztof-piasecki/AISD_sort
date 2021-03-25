62c15399d5aed89061914fd7680adc1642f22cf8
Krzysztof <krzysztof.piasecki1410@gmail.com>
2021-03-25 17:24:17 +0100

    Hotfix#2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // TO GET DATA TO EXCEL YOU NEED TO CHANGE quantity and loops value to your chosen number (it has to be absolute positive number)
+
+        //it is a number of elements that have to be sorted
         int quantity = 0;
+        //it is a number of times the quantity will be multiply by
+        // lets say quantity starts at 10 and loops are 3
+        //the first number of elements will be 10, following one will be 20
+        //and the final will be 30 etc.
         int loops = 0;
         var exportExcel = new ExcelExport();
 
