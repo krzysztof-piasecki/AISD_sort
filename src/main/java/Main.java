f8bce36935957e6b6407e1f2cc61329c2ca201e8
Krzysztof <krzysztof.piasecki1410@gmail.com>
2021-03-25 17:16:28 +0100

    Hotfix#1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,28 @@
 import exporter.ExcelExport;
 import generator.NumbersGenerator;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int quantity = 4000;
-        int loops = 20;
+        int quantity = 0;
+        int loops = 0;
         var exportExcel = new ExcelExport();
-
 
 
         exportExcel.exportToExcel(sortElementsWithLoops(loops, quantity));
     }
 
-    public static List<String[]> sortElementsWithLoops(int loops, int quantity){
+    public static List<String[]> sortElementsWithLoops(int loops, int quantity) {
         List<String[]> allData = new ArrayList<>();
         var gen = new NumbersGenerator();
-        var max = 10;
-        var min = 2;
 
-        //To obtain various outcomes, we will use random (max and min are ranges between which we want to create random number)
-        var randomMultiplier = Math.random()* (max - min) + min;
-
-        for (int i = 1; i <= loops; i++){
-            allData.addAll(gen.getAllRandomAndSortTime(quantity * i * ((int) Math.floor(randomMultiplier))));
+        for (int i = 1; i <= loops; i++) {
+            allData.addAll(gen.getAllRandomAndSortTime(quantity * i));
+            double percentage = ((double)i / (double)loops) * 100;
+            System.out.println("Loading " + new DecimalFormat("#,##0").format(percentage) + "%");
         }
         System.out.println("Sortowanie zakończone, dane zostały wyeksportowane do excela");
         return allData;
